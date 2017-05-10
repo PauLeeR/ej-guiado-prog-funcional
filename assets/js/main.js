09bafc82a21b81c72a57abe205abc3e0ea064fdf
Paulina Rojas <lindaliel@gmail.com>
2017-05-10 13:02:18 -0300

    ej guiado 2 computadores, manera imperativa

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,11 +19,56 @@ document.write(intersection(l1, l2));*/
 //intersection(l1, l2);
 
 //forma declarativa, funcional, utilizando funciones
-var l1=[1,2,3];
+/*var l1=[1,2,3];
 var l2=[2,3,4];
 function intersectionSet2(l1, l2){
 	return l1.filter(function(n){
 		return l2.indexOf(n) !== -1
 	});
 };
-document.write(intersectionSet2(l1, l2));
+document.write(intersectionSet2(l1, l2));*/
+
+//Ejercicio guiado #2, hay que obtener los elementos del arreglo que sean "computadores"
+//Forma imperativa 
+
+var productos = [
+   {
+	nombre: 'pijama',
+	tipo: 'ropa'
+},
+   {
+	nombre: 'nevera',
+	tipo: 'electrodomestico'
+},
+   {
+	nombre: 'asus',
+	tipo: 'computador'
+},
+   {
+	nombre: 'macbook',
+	tipo: 'computador'
+},
+   {
+	nombre: 'pijama',
+	tipo: 'ropa'
+},
+   {
+	nombre: 'toshiba',
+	tipo: 'computador'
+},
+];
+
+var computadores = [];
+for(var i=0; i<productos.length; i++){
+	if (productos[i].tipo === 'computador'){
+		computadores.push(productos[i]);
+	}
+}
+console.log(computadores)
+
+/*var computadores = productos.filter(function(producto){
+	return producto.tipo === 'computador';
+});
+console.log(computadores);*/
+
+
